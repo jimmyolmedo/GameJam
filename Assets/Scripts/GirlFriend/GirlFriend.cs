c7290b89c83403e511862e291a05a96fb752b940
jimmyolmedo <103449835+jimmyolmedo@users.noreply.github.com>
2025-04-04 02:42:24 -0300

    girlfriend pseudocodigo

---
+++ Assets/Scripts/GirlFriend/GirlFriend.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GirlFriend : MonoBehaviour
+{
+    //variables
+
+    //tiempo que espera a que el jugador llegue a ella
+
+    //evento de cSharp en el que se pueda suscribir el spawner para contar
+
+    //method
+
+    //funcion para cuando el jugador llega a ella antes de que se acabe el tiempo, hace que el spawner sume un contador para spawnear y luego se cambiara a otra ubicacion
+
+    //funcion para cuando el jugador no llegue a tiempo, se le quitara vida y se cambiara de ubicacion
+}
